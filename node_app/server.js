79a9a2d73af43a6b01087c201b063ec88bc4082f
Muhammad Humair <muhammadhumair001@gmail.com>
2023-04-17 06:45:54 +0500

    Update server.js

--- node_app/server.js
+++ node_app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HA HA HA');
+  res.send('Hello World AGAIN HAHAHAH');
 });
 
 app.listen(PORT, HOST, () => {
