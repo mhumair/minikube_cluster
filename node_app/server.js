d467f7dab219e5f1640e6daae47ee499034f42a1
mhumair7 <muhammadhumair001@gmail.com>
2023-04-17 07:21:05 +0500

    testing hooks

--- node_app/server.js
+++ node_app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HA HA HA');
+  res.send('This is git hooks by example');
 });
 
 app.listen(PORT, HOST, () => {
