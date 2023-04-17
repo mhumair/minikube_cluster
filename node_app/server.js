9a0e1c50b130243137ceec21f15020fcacbc5d7f
mhumair7 <muhammadhumair001@gmail.com>
2023-04-17 07:27:36 +0500

    testing hooks

--- node_app/server.js
+++ node_app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Testing again :)');
+  res.send('This is unreal');
 });
 
 app.listen(PORT, HOST, () => {
